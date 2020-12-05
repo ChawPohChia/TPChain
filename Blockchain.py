6c25a1c1bca4c330a160b20ead0afa43c7b537b2
cpchia <chawchia@hotmail.co.uk>
2020-12-05 23:21:27 +0800

    Implement GetMiningJob() and GenerateGenesisBlock() for blockchain

--- Blockchain.py
+++ Blockchain.py
@@ -1,15 +1,35 @@
+from Block import Block
+import copy
+
 class Blockchain:
+
     def __init__(self):
+        self.blocks = []
+        self.lastblock=None
+        self.miningJob = None
         self.pendingTransactions=[]
+        self.currentDifficulty = 3
+        self.generateGenesisBlock()
 
     def addTransaction(self, transaction):
         self.pendingTransactions.append(transaction)
 
+    def generateGenesisBlock(self):
+        #def __init__(self, index, transactions, prevBlockHash, difficulty):
+        genesisBlock = Block(0, None, None, None)
+        self.blocks.append(genesisBlock)
+        self.lastblock=genesisBlock
 
-
-
-    ##Blocks =""
-    ##PendingTransactions =""
-    ##currentDifficulty=0
-    ##MiningJobs={}
+    def getMiningJob(self):
+        #Let miner to get the blocktomine, only start next block when the current job/block mined
+        if (self.miningJob==None):
+            transactionsInBlock = copy.deepcopy(self.pendingTransactions)
+            nextBlockindex = self.lastblock.index + 1
+            prevBlockHash = self.lastblock.blockHash
+            # (self, index, transactions, prevBlockHash):
+            blockToMine = Block(nextBlockindex, transactionsInBlock, prevBlockHash, self.currentDifficulty)
+            self.miningJob.append(blockToMine)
+            self.pendingTransactions = []  # clear up pending transaction after txs are included in prevBlockHash the block.
+        else:
+            return self.miningJob;
 
