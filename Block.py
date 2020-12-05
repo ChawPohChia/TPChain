a9847575628ca35a85f7b3cdd26be784b511b6c1
cpchia <chawchia@hotmail.co.uk>
2020-12-05 23:20:11 +0800

    Implement block and include transaction in it

--- Block.py
+++ Block.py
@@ -1,11 +1,46 @@
+import time
+from hashlib import sha256
+import json
+
+# Index =""
+# Transactions =""
+# Difficulty=0
+# PrevBlockHash={}
+# MinedBy=""
+# BlockDataHash=""
+# Nonce=0
+# DateCreated=""
+
 class Block:
-    Index =""
-    Transactions =""
-    Difficulty=0
-    PrevBlockHash={}
-    MinedBy=""
-    BlockDataHash=""
-    Nonce=0
-    DateCreated=""
-    BlockHash=""
+    def __init__(self, index, transactions, prevBlockHash, difficulty):
+        self.dateCreated = time.time()
+        self.difficulty = difficulty
+
+        self.index=index
+        self.transactions = transactions
+        self.prevBlockHash = prevBlockHash
+
+        #blockData = json.dumps(self.transactions, default=obj_dict)  [ob.__dict__ for ob in list_name]
+        if (index==0):
+            blockData=""
+        else:
+            blockData = json.dumps([ob.__dict__ for ob in self.transactions])
+
+        self.blockDataHash = sha256(blockData.encode()).hexdigest()
+        self.nonce = 0
+        self.minedBy=None;
+        self.blockHash = None;
+
+
+
+
+
+
+
+
+
+
+
+
+    #BlockHash=""
 
--- README.md
+++ README.md
@@ -6,3 +6,4 @@ References:
 1) https://www.freecodecamp.org/news/how-to-build-a-web-application-using-flask-and-deploy-it-to-the-cloud-3551c985e492/
 2) https://www.activestate.com/blog/how-to-build-a-blockchain-in-python/
 3) https://flask-cors.readthedocs.io/en/latest/
+4) MI1: Buildng your own Blockchain -Exercises
