3a46e1c8367d11c4ba1e63e2b44326773c232e00
cpchia <chawchia@hotmail.co.uk>
2021-02-04 06:04:53 +0800

    Request Greediness Logic Test

--- TestingToBeDeleted.py
+++ TestingToBeDeleted.py
@@ -1,86 +1,29 @@
 ## testing to filrer python list
 from Transaction import Transaction
 import copy
-import datetime
+from datetime import datetime as dt, timedelta
 
-"""
-#'{"from":"1Nh1qKQQ7q8Etsu5QndtQUkDHk5oiv8zKr","to":"XxnoCyJMtY323Y7mG6ePWtAmCoTH7KGqxX","value":100000,"fee":100,"dateCreated":"2020-12-13 16:27:34"}'
-def strToDatetime(dateString):
-    return datetime.datetime.strptime(dateString, '%Y-%m-%d %H:%M:%S')
+print(dt.now())
+#currentRequestDate="2021-02-02 16:4:38"
+currentRequestDate=dt.now().strftime("%Y-%m-%d %H:%M:%S")
+print("current request time:"+currentRequestDate)
+requestRecords = {"abcabc": ["2021-01-31 16:14:39","2021-02-01 23:4:39","2021-02-03 16:4:37", "2021-02-04 03:14:38", "2021-3-31 16:14:39"],"cdecde": ["2020-12-4 16:4:38", "2020-12-5 16:4:38"]}
 
-t1=Transaction(data={"from":"1Nh1qKQQ7q8Etsu5QndtQUkDHk5oiv8zKr","to":"XxnoCyJMtY323Y7mG6ePWtAmCoTH7KGqxX","value":100000,"fee":100,"dateCreated":"2020-12-13 18:27:34"}, senderPubKey="", transactionDataHash="",senderSignature="")
-t2=Transaction(data={"from":"2Nh1qKQQ7q8Etsu5QndtQUkDHk5oiv8zKr","to":"XxnoCyJMtY323Y7mG6ePWtAmCoTH7KGqxX","value":100000,"fee":100,"dateCreated":"2020-12-13 17:27:34"}, senderPubKey="", transactionDataHash="",senderSignature="")
-t3=Transaction(data={"from":"3Nh1qKQQ7q8Etsu5QndtQUkDHk5oiv8zKr","to":"XxnoCyJMtY323Y7mG6ePWtAmCoTH7KGqxX","value":100000,"fee":100,"dateCreated":"2020-12-13 16:27:34"}, senderPubKey="", transactionDataHash="",senderSignature="")
-t4=Transaction(data={"from":"4Nh1qKQQ7q8Etsu5QndtQUkDHk5oiv8zKr","to":"XxnoCyJMtY323Y7mG6ePWtAmCoTH7KGqxX","value":100000,"fee":100,"dateCreated":"2020-12-13 17:27:34"}, senderPubKey="", transactionDataHash="",senderSignature="")
-t5=Transaction(data={"from":"4Nh1qKQQ7q8Etsu5QndtQUkDHk5oiv8zKr","to":"XxnoCyJMtY323Y7mG6ePWtAmCoTH7KGqxX","value":100000,"fee":100,"dateCreated":"2020-12-13 16:27:34"}, senderPubKey="", transactionDataHash="",senderSignature="")
-t6=Transaction(data={"from":"4Nh1qKQQ7q8Etsu5QndtQUkDHk5oiv8zKr","to":"XxnoCyJMtY323Y7mG6ePWtAmCoTH7KGqxX","value":100000,"fee":100,"dateCreated":"2020-12-13 12:27:34"}, senderPubKey="", transactionDataHash="",senderSignature="")
-t7=Transaction(data={"from":"2Nh1qKQQ7q8Etsu5QndtQUkDHk5oiv8zKr","to":"XxnoCyJMtY323Y7mG6ePWtAmCoTH7KGqxX","value":100000,"fee":100,"dateCreated":"2020-11-13 13:27:34"}, senderPubKey="", transactionDataHash="",senderSignature="")
+for date in requestRecords["abcabc"]:
+    history= dt.strptime(date, "%Y-%m-%d %H:%M:%S")
+    current = dt.strptime(currentRequestDate, "%Y-%m-%d %H:%M:%S")
+    print((current-history).days<1)
 
-pendingTransactions=[t1,t2,t3,t4,t5,t6,t7]
-uniqueTransactions=[]
-for ptx in pendingTransactions:
-    toSkip = False;
-    for element in pendingTransactions:
-        if ((ptx.data["from"] == element.data["from"]) & (strToDatetime(ptx.data["dateCreated"]) > strToDatetime(element.data["dateCreated"]))):
-            toSkip=True;
-    if (not toSkip):
-        uniqueTransactions.append(ptx)
-
-for tx in uniqueTransactions:
-   pendingTransactions.remove(tx)
-
-print("Checking..")
-"""
-
-"""
-pendingTransactions=[t1,t2,t3,t4,t5, t6, t7]
-TransToProcess=[]
-#pendingTransactions_Temp=copy.deepcopy(pendingTransactions)
-TransToProcess.append(pendingTransactions[0])
-#pendingTransactions.remove(TransToProcess[0])
+#requestRecords["abcabc"].append("2020-12-5 16:4:38")
 
 
-for ptx in pendingTransactions:
-    for ttp in TransToProcess: # checking this pending transaction is with same name in ttp
-        # Only one "from" address's transaction in one block, if more, only allow the earliest
-        if((ptx.data["from"]==ttp.data["from"])):
-            if (strToDatetime(ptx.data["dateCreated"]) < strToDatetime(ttp.data["dateCreated"])):
-                TransToProcess.append(ptx)
-                #pendingTransactions.append(ttp)
-                TransToProcess.remove(ttp)
-            else:
-                continue
-    if ptx not in TransToProcess:
-        TransToProcess.append(ptx)
-    #pendingTransactions.remove(ptx)
+#for date in requestRecords["abcabc"]:
+#    print(date)
 
-for tx in TransToProcess:
-   pendingTransactions.remove(tx)
-"""
-
-#test2
-t1=Transaction(data={"from":"1Nh1qKQQ7q8Etsu5QndtQUkDHk5oiv8zKr","to":"XxnoCyJMtY323Y7mG6ePWtAmCoTH7KGqxX","value":100000,"fee":100,"dateCreated":"2020-12-13 18:27:34"}, senderPubKey="", transactionDataHash="",senderSignature="")
-t2=Transaction(data={"from":"2Nh1qKQQ7q8Etsu5QndtQUkDHk5oiv8zKr","to":"XxnoCyJMtY323Y7mG6ePWtAmCoTH7KGqxX","value":100000,"fee":100,"dateCreated":"2020-12-13 17:27:34"}, senderPubKey="", transactionDataHash="",senderSignature="")
-t3=Transaction(data={"from":"3Nh1qKQQ7q8Etsu5QndtQUkDHk5oiv8zKr","to":"XxnoCyJMtY323Y7mG6ePWtAmCoTH7KGqxX","value":100000,"fee":100,"dateCreated":"2020-12-13 16:27:34"}, senderPubKey="", transactionDataHash="",senderSignature="")
-t4=Transaction(data={"from":"4Nh1qKQQ7q8Etsu5QndtQUkDHk5oiv8zKr","to":"XxnoCyJMtY323Y7mG6ePWtAmCoTH7KGqxX","value":100000,"fee":100,"dateCreated":"2020-12-13 17:27:34"}, senderPubKey="", transactionDataHash="",senderSignature="")
-t5=Transaction(data={"from":"4Nh1qKQQ7q8Etsu5QndtQUkDHk5oiv8zKr","to":"XxnoCyJMtY323Y7mG6ePWtAmCoTH7KGqxX","value":100000,"fee":100,"dateCreated":"2020-12-13 16:27:34"}, senderPubKey="", transactionDataHash="",senderSignature="")
-t6=Transaction(data={"from":"4Nh1qKQQ7q8Etsu5QndtQUkDHk5oiv8zKr","to":"XxnoCyJMtY323Y7mG6ePWtAmCoTH7KGqxX","value":100000,"fee":100,"dateCreated":"2020-12-13 12:27:34"}, senderPubKey="", transactionDataHash="",senderSignature="")
-t7=Transaction(data={"from":"2Nh1qKQQ7q8Etsu5QndtQUkDHk5oiv8zKr","to":"XxnoCyJMtY323Y7mG6ePWtAmCoTH7KGqxX","value":100000,"fee":100,"dateCreated":"2020-11-13 13:27:34"}, senderPubKey="", transactionDataHash="",senderSignature="")
-
-pendingTransactions=[t1,t2,t3,t4,t5, t6, t7]
-allTransactions=[t1,t2,t3,t4,t5, t6, t7]
-
-t2.setTransferSuccessful(True)
-
-print("AllTransaction")
-for ptx in allTransactions:
-    print(ptx.getStatus())
-
-print("PendingTransaction")
-for ptx in pendingTransactions:
-    print(ptx.getStatus())
-
-
+#Please refer to https://www.programiz.com/python-programming/datetime/strftime
+#a = dt.strptime("2013-12-12 16:4:38", "%Y-%m-%d %H:%M:%S")
+#b = dt.strptime("2013-10-15", "%Y-%m-%d")
+#print(a - b)
 
 
 
